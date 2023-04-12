89b1006fa263131c13bbf31e640ed4c6acb02399
kokiasaka <129828586+kokiasaka@users.noreply.github.com>
2023-04-12 20:45:05 +0900

    Revert "task2_一次コミット"

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,11 +42,8 @@ public class LoginController {
 		UserInfo selectedUserInfo = usersService.selectUserInfo(email, password);
 
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
-		if (!(ObjectUtils.isEmpty(selectedUserInfo))) {
-			return "redirect:/home";
-		} else {
-			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが存在しません");
-			return "login";
-		}
+
+		
+		return "redirect:/home";
 	}
 }
